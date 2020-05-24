3ea7fc0ba33e53823e0ebd2eb2a5b8cc5d484ba7
Артем Крылов <kav128@outlook.com>
2020-05-25 00:06:12 +0300

    Change model

--- fit.py
+++ fit.py
@@ -4,12 +4,15 @@ import numpy as np
 import keras
 from keras.utils import to_categorical
 from keras.callbacks import TensorBoard
-from keras.models import Model
-from keras.layers import Input, Dense, Dropout, Flatten, GaussianDropout, LSTM, Bidirectional
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Dropout, Flatten, GaussianDropout, LSTM, Bidirectional, BatchNormalization, MaxPooling1D, MaxPooling2D, MaxPooling3D, AveragePooling1D
 import keras_metrics as km
 import datetime
+from sklearn.model_selection import train_test_split
 
 from edf_preprocessor import EDF_Preprocessor
+from ind_rnn import IndRNN
+from ind_rnn import IndRNNCell, RNN
 
 
 # Не работает под Windows. Под Docker пока не проверял
@@ -18,25 +21,38 @@ from edf_preprocessor import EDF_Preprocessor
 
 # BEGIN MODEL DESCRIPTION
 
-inputs = Input(shape=(256,23))
-x = GaussianDropout(0.1)(inputs)	#generating noise during training for better generalization
-
-x = LSTM(256)(x)
-x = Dropout(0.2)(x)
-x = Dense(256)(x)
-
-x = Dropout(0.2)(x)
-x = Dense(256)(x)
-
-x = Dropout(0.2)(x)
-
+ip = Input(shape=(256,23))
+x = IndRNN(512, return_sequences=True)(ip)
+x = BatchNormalization()(x)
+x = AveragePooling1D()(x)
+x = IndRNN(512, return_sequences=True)(x)
+x = BatchNormalization()(x)
+x = AveragePooling1D()(x)
+x = IndRNN(512, return_sequences=True)(x)
+x = BatchNormalization()(x)
+x = AveragePooling1D(2,2)(x)
+x = IndRNN(256, return_sequences=True)(x)
+x = BatchNormalization()(x)
+x = AveragePooling1D()(x)
+x = IndRNN(256, return_sequences=True)(x)
+x = BatchNormalization()(x)
+x = AveragePooling1D()(x)
+x = IndRNN(128, return_sequences=True)(x)
+x = BatchNormalization()(x)
+x = AveragePooling1D()(x)
+x = IndRNN(128, return_sequences=True)(x)
+x = BatchNormalization()(x)
+x = AveragePooling1D()(x)
+x = AveragePooling1D()(x)
+x = IndRNN(128, return_sequences=False)(x)
+x = Dense(128)(x)
+x = Dense(128)(x)
 predictions = Dense(units=3, activation="softmax")(x)
+model = Model(ip, predictions)
 
-model = Model(inputs, predictions)
+adam = keras.optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-3, decay=0, amsgrad=False)
 
-# adam = keras.optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-3, decay=0.0, amsgrad=False)
-rmsprop = keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9)
-model.compile(rmsprop, loss='categorical_crossentropy', metrics=['accuracy', km.precision(label=1), km.recall(label=1)])
+model.compile(adam, loss='categorical_crossentropy', metrics=['accuracy' , km.precision(label=1) , km.recall(label=1)])
 
 # END MODEL DESCRIPTION
 
@@ -59,7 +75,13 @@ y = to_categorical(y, num_classes=3)
 
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard = TensorBoard(log_dir=log_dir)
-model.fit(X, y, epochs=15, verbose=1, validation_split=0.25, batch_size=256, callbacks=[tensorboard])
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_binary_train = to_categorical(y_train)
+y_binary_test = to_categorical(y_test)
+
+model.fit(X_train, y_binary_train, batch_size=256, epochs=40, verbose=1, validation_split=0.15, callbacks=[tensorboard])
 del X, y
 
 print('Loading data for training...')
